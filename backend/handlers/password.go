65254b795e98cd0e9a2b4bfccf98932deb778358
Aarav Arora <aaravarora.works@gmail.com>
2022-11-08 15:37:25 +0530

    feat: forget password and change password routes to be implemented

---
+++ backend/handlers/password.go
@@ -0,0 +1,24 @@
+package handlers
+
+// import (
+// 	"github.com/axrav/Systopher/backend/types"
+// 	"github.com/gofiber/fiber/v2"
+// )
+
+// func Forgetpassword(c *fiber.Ctx) error {
+// 	email := new(types.Email)
+// 	if err := c.BodyParser(email); err != nil {
+// 		return c.Status(500).JSON(fiber.Map{
+// 			"message": "Internal server error",
+// 		})
+// 	}
+// 	if email.Email == "" {
+// 		return c.Status(400).JSON(fiber.Map{
+// 			"message": "Missing email",
+// 		})
+// 	}
+// 	return c.JSON(fiber.Map{
+// 		"message": "Email sent",
+// 	})
+
+// }
