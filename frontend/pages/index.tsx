505927b3c22d3dad03d1d432806c44e596218b98
Ojas Tyagi <devmegablaster@gmail.com>
2022-11-08 22:43:30 +0530

    fix: landing coming soon

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,10 +6,10 @@ const Home: NextPage = () => {
   return (
     <div className="h-screen w-screen overflow-auto bg-gray-800">
       <div className="h-full w-full flex flex-col space-y-10 items-center justify-center">
-        <h1 className="text-white md:text-4xl text-2xl font-mono font-light">
+        <h1 className="text-white md:text-4xl text-3xl font-mono font-light">
           Systopher
         </h1>
-        <h1 className="text-white md:text-7xl text-3xl">Coming Soon!</h1>
+        <h1 className="text-white md:text-7xl text-4xl">Coming Soon!</h1>
       </div>
     </div>
   );
